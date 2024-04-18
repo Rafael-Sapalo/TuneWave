6375a6abcae93e4e59218e7089db702175029bd5
my_little_coder <114910627+Rafael-Sapalo@users.noreply.github.com>
2024-04-19 00:31:21 +0200

    [FIX]: fixing the typing of the statusCode
    -- In this commit --
    
    - The statusCode was not typed correctly, so I fixed it.

--- server/src/routes/router.service.ts
+++ server/src/routes/router.service.ts
@@ -2,6 +2,7 @@ import { Hono } from "hono";
 import { logger } from "hono/logger";
 import { AuthService } from "./auth/auth.service";
 import { poweredBy } from "hono/powered-by";
+import { statusCodes } from "../utils/statusCodes";
 
 export class RouterService {
     private app = new Hono();
@@ -24,13 +25,12 @@ export class RouterService {
         this.app.route(`${this.path}/auth`, this.authRouter);
         this.app.get(`${this.path}/`, async (ctx) => {
             return ctx.json({
-                message: "Hello, World!"
-            }, 200);
+                message: "Hello, World!",
+            }, statusCodes.C200.OK);
         });
     }
 
     public getApp() {
         return this.app;
     }
-
 }
