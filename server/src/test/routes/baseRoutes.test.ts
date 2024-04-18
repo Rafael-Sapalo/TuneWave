5db755a152daf7d237ece81a13520f77a5311716
my_little_coder <114910627+Rafael-Sapalo@users.noreply.github.com>
2024-04-18 11:50:01 +0200

    [ADD]: new test case for the base route
    -- In this commit --
    
    - Added a new test case for the base route wich handles 404 errors

--- server/src/test/routes/baseRoutes.test.ts
+++ server/src/test/routes/baseRoutes.test.ts
@@ -10,4 +10,10 @@ describe('Basse Routes', () => {
         expect(res.status).toBe(statusCodes.C200.OK)
         expect(await res.json()).toEqual({ message: "Hello, World!" }) 
     })
+
+    test('GET /api/404', async () => {
+        const res = await app.request('/api/404')
+        expect(res.status).toBe(statusCodes.C400.NOT_FOUND)
+        expect(await res.text()).toEqual("404 Not Found" ) 
+    })
 })
