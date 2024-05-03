b1007612ed433bb0431e5ec9338136e39d07e663
my_little_coder <114910627+Rafael-Sapalo@users.noreply.github.com>
2024-05-04 00:52:36 +0200

    [ADD]: new coding style for the project

--- server/src/routes/auth/auth.controllers.ts
+++ server/src/routes/auth/auth.controllers.ts
@@ -1,30 +1,26 @@
-import { registerErrorMessagesArray } from "../../utils/error.message";
+import { authErrorMessage } from "../../utils/error.message";
+import { UserLogSchema, userSchema } from "../../utils/zod.schema";
 import { isStrongPassword } from '../../utils/checktypes';
 import { UserTable } from '../../config/schema/db.schema';
 import { statusCodes } from '../../utils/statusCodes';
-import { UserLogSchema, userSchema } from "../../utils/zod.schema";
 import { zValidator } from "@hono/zod-validator";
 import { passwordSec } from '../../utils/Types';
 import { createFactory } from "hono/factory";
 import { db } from '../../config/db';
-import { z } from "zod";
 import { eq } from "drizzle-orm";
-import { password } from "bun";
 
 export class AuthController {
     private authFactory = createFactory();
 
     readonly registerFactory = this.authFactory.createHandlers(zValidator('json', userSchema, (value, ctx) => {
-        if (value.data.password.length < 8) {
-            return ctx.json({ message: registerErrorMessagesArray[0] }, statusCodes.C400.BAD_REQUEST);
-        }
-        if (!isStrongPassword(value.data.password)) {
-            return ctx.json({ message: registerErrorMessagesArray[1] }, statusCodes.C400.BAD_REQUEST);
-        }
+        if (value.data.password.length < 8)
+            return ctx.json({ message: authErrorMessage[0] }, statusCodes.C400.BAD_REQUEST);
+        if (!isStrongPassword(value.data.password))
+            return ctx.json({ message: authErrorMessage[1] }, statusCodes.C400.BAD_REQUEST);
     }), 
     async (ctx) => {
         const body = ctx.req.valid('json');
-        const user = await db.insert(UserTable).values(
+        const user: unknown = await db.insert(UserTable).values(
             {
                 email: body.email.toString().toLowerCase(),
                 username: body.username.toString(),
@@ -37,17 +33,13 @@ export class AuthController {
     readonly loginFactory = this.authFactory.createHandlers(zValidator('json', UserLogSchema), async (ctx) => {
         const body = ctx.req.valid('json');
         const user = await db.select({email: UserTable.email, password: UserTable.password}).from(UserTable).where(eq(UserTable.username, body.username.toString()));
-        if (!user) return ctx.json({ message: 'User not found' }, statusCodes.C400.NOT_FOUND);
+        if (!user) return ctx.json({ message: authErrorMessage[2] as string }, statusCodes.C400.NOT_FOUND);
         const compare = await passwordSec.comparing(body.password.toString(), user[0].password);
-        if (!compare) return ctx.json({ message: 'Invalid password' }, statusCodes.C400.BAD_REQUEST);
-        return ctx.json({
-            message: 'User logged in',
-        }, statusCodes.C200.OK);
+        if (!compare) return ctx.json({ message: authErrorMessage[3] }, statusCodes.C400.BAD_REQUEST);
+        return ctx.json({ message: 'User logged in' }, statusCodes.C200.OK);
     });
 
     readonly logoutFactory = this.authFactory.createHandlers(async (ctx) => {
-        return ctx.json({
-            message: 'Log Out'
-        })
+        return ctx.json({ message: 'Log Out' });
     })
 }
--- server/src/utils/error.message.ts
+++ server/src/utils/error.message.ts
@@ -1,6 +1,8 @@
-export const registerErrorMessagesArray: string[] = [
+export const authErrorMessage: string[] = [
     "Password must be at least 8 characters long",
-    "Password must contain at least one uppercase letter, one lowercase letter, one number and one special character"
+    "Password must contain at least one uppercase letter, one lowercase letter, one number and one special character",
+    "User not found",
+    "Invalid password",
 ];
 
 export const loginErrorMessagesArray: string[] = [
