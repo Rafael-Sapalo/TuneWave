a492c0e55265032ea7a6bad5fbedaf9393628498
my_little_coder <114910627+Rafael-Sapalo@users.noreply.github.com>
2024-05-12 22:36:09 +0200

    [ADD]: delete user for testing

--- server/src/routes/router.service.ts
+++ server/src/routes/router.service.ts
@@ -1,3 +1,5 @@
+import { LibraryService } from "./library/library.service";
+import { UserTable } from "../config/schema/db.schema";
 import { HTTPException } from "hono/http-exception";
 import { statusCodes } from "../utils/statusCodes";
 import { AuthService } from "./auth/auth.service";
@@ -7,9 +9,10 @@ import { db } from "../config/db";
 import { Hono } from "hono";
 
 export class RouterService {
-    private app = new Hono();
+    private app = new Hono().basePath('/api');
     private authRouter = new AuthService().getAuth();
-    private readonly baseRoute = `/api`;
+    private libraryRouter = new LibraryService().LibInstance();
+    private readonly baseRoute = `/v1`;
 
     constructor() {
         this.initGlobalMiddleware();
@@ -19,7 +22,7 @@ export class RouterService {
     private initGlobalMiddleware() {
         this.app.use(logger());
         this.app.use(poweredBy());
-        this.app.notFound(async (ctx) => {
+        this.app.notFound((ctx) => {
             return ctx.json({ message: "Not found" }, statusCodes.C400.NOT_FOUND);
         });
         this.app.onError((err, ctx) => {
@@ -30,28 +33,34 @@ export class RouterService {
     }
 
     private initRoutes() {
-
-        this.app.route(`${this.baseRoute}/auth`, this.authRouter);
-        this.app.get(`${this.baseRoute}/getAllUsers`, async (ctx) => {
-            const users = await db.transaction(async (trx) => {
-                const users = await db.query.UserTable.findMany();
-                if (!users) {
-                    return trx.rollback();
-                }
-                return users;
-            });
-            if (!users) {
+        this.app.route(`${this.baseRoute}`, this.authRouter);
+        this.app.route(`${this.baseRoute}`, this.libraryRouter);
+        this.app.delete(`${this.baseRoute}/deleteUser`, async (ctx) => {
+            try {
+                await db.delete(UserTable);
+            } catch (error) {
+                if (error instanceof Error)
+                    throw new HTTPException(statusCodes.C500.INTERNAL_SERVER_ERROR, { message: error.message });
                 return ctx.json({ message: "No users found" }, statusCodes.C400.NOT_FOUND);
             }
+            return ctx.json({ message: "All users deleted" }, statusCodes.C200.OK);
+        });
+        this.app.get(`${this.baseRoute}/getAllUsers`, async (ctx) => {
+            let users;
+            try {
+                users = await db.query.UserTable.findMany();
+            } catch (error) {
+                if (error instanceof Error)
+                    throw new HTTPException(statusCodes.C500.INTERNAL_SERVER_ERROR, { message: error.message });
+                return ctx.json({ message: "No users found" }, statusCodes.C400.NOT_FOUND);
+            }
+            if (users.length === 0) return ctx.json({ message: "No users found" }, statusCodes.C400.BAD_REQUEST);
             return ctx.json({
-                message: "All users",
-                User: users,
+                message: "All users", User: users,
             }, statusCodes.C200.OK);
         });
         this.app.get(`${this.baseRoute}/`, async (ctx) => {
-            return ctx.json({
-                message: "Hello, World!",
-            }, statusCodes.C200.OK);
+            return ctx.json({ message: "Hello, World!" }, statusCodes.C200.OK);
         });
     }
 
